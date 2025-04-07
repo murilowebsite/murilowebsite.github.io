ce4222165764720834208983a03e49ae8f50bf55
murilowebsite <dagostimalexsandro@gmail.com>
2025-04-07 17:18:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
-      var click = 0;
-      var clickinicial = 1;
-      var preco1 = 10;
-      var preco2 = 50;
-      var points = document.getElementsByClassName('pontos')[0];
-      function on() {
-        var caixa = document.getElementsByClassName('caixa')[0];
-        caixa.style.opacity = '1';
-      }
-      function off() {
-        var caixa = document.getElementsByClassName('caixa')[0];
-        caixa.style.opacity = '0';
-      }
-      function botaoclick() {
-       click += clickinicial;
+var click = 0;
+var clickinicial = 1;
+var preco1 = 10;
+var preco2 = 50;
+var points = document.getElementsByClassName('pontos')[0];
+var loop = false;
+
+function on() {
+  var caixa = document.getElementsByClassName('caixa')[0];
+  caixa.style.opacity = '1';
+}
+
+function off() {
+  var caixa = document.getElementsByClassName('caixa')[0];
+  caixa.style.opacity = '0';
+}
+
+function botaoclick() {
+  click += clickinicial;
+  points.innerText = click;
+}
+
+function item1() {
+  if (click >= preco1) {
+    clickinicial += 1;
+    click -= preco1;
+    preco1 += 50;
+    points.innerText = click;
+  }
+}
+
+function item2() {
+  if (click >= preco2 && !loop) {
+    click -= preco2;
+    preco2 += 100;
+    loop = true; // Ativar o loop
+    points.innerText = click;
+    loopclicker();
+  }
+}
+
+function loopclicker() {
+  if (loop) {
+    setTimeout(function () {
+      click += clickinicial;
       points.innerText = click;
-      }
-      function item1(){
-        if (click >= preco1){
-        clickinicial += 1;
-        click -= preco1;
-        preco1 += 50;
-        points.innerText = click;
-        }
-      }
-      function item2(){
-            if(click >= preco2){
-              click -= preco2;
-              preco2 += 100
-              var loop = true;
-              points.innerText = click;
-              var itemcomprado = true;
-            }
-            if(itemcomprado){
-                  loopclicker();
-            }
-      }
-function loopclicker(){
-      while(loop){
-      setTimeout(1000)
-      click += clickinicial
-      }
+      loopclicker(); 
+    }, 1000);
+  }
 }
