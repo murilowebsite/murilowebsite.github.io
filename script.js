d9d485906ba76d82bd95b17591cd423cdb149b6e
murilowebsite <dagostimalexsandro@gmail.com>
2025-04-11 13:46:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ var click = 0;
 var clickinicial = 1;
 var preco1 = 10;
 var preco2 = 50;
-var points = document.getElementsByClassName('pontos');
+var points = document.getElementsByClassName('pontos')[0]; 
 var loop = false;
 var preco3 = 500;
 var loop1 = false;
-var musicacarlos = getElementsByClassName('musica')
-  
+var musicacarlos = document.getElementsByClassName('musica'); 
+
 function on() {
-  var caixa = document.getElementsByClassName('caixa');
+  var caixa = document.getElementsByClassName('caixa')[0]; 
   caixa.style.opacity = '1';
 }
 
 function off() {
-  var caixa = document.getElementsByClassName('caixa');
+  var caixa = document.getElementsByClassName('caixa')[0]; 
   caixa.style.opacity = '0';
 }
 
 function botaoclick() {
   click += clickinicial;
-  points.innerText = click;
+  points.innerText = click; 
 }
 
 function item1() {
@@ -28,36 +28,6 @@ function item1() {
     clickinicial += 1;
     click -= preco1;
     preco1 += 50;
-    points.innerText = click;
+    points.innerText = click; 
   }
-}
-
-function item2() {
-  if (click >= preco2 && !loop) {
-    click -= preco2;
-    loop = true; 
-    points.innerText = click;
-    loopclicker();
-  }
-}
-
-function loopclicker() {
-  if (loop) {
-    setTimeout(function (){
-      click += clickinicial;
-      points.innerText = click;
-      loopclicker(); 
-    }, 1000);
-  }
-}
-function item3(){
-  if(click >= preco3 && !loop1) {
-    click -= preco3;
-    loop1 = true;
-    points.innerText = click;
-    loopcarlos();
-  }
-}
-function loopcarlos() setTimeout{
-  musicacarlos.play()
 }
